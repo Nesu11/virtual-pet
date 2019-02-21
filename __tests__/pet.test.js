3dc6d1fd4cb04b8167d0116bb828fa97c6c08d45
Nesu11 <anesujane98@gmail.com>
2019-02-21 21:05:10 +0000

    pet constructor

---
+++ __tests__/pet.test.js
@@ -0,0 +1,10 @@
+const Pet = require('../src/pet')
+describe('constructor', () => {
+
+    it('returns an object', () => {
+  
+      expect(new Pet('Fido')).toBeInstanceOf(Object);
+  
+    });
+  
+  });
